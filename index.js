ac47e8af2d0602be9f469dc5d27923c9a3f7ee7f
lukaas33 <Lucasvanosenbruggen@gmail.com>
2017-08-03 17:01:51 +0200

    Changed index to public

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var app = express();
 
 // Port
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static([__dirname, '/public'].join()));
 
 // Routes
-// app.set('index', [__dirname, '/index'].join());
+app.set('index', [__dirname, '/index.html'].join());
 app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
