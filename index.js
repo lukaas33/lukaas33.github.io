488d9ebb6f3918334618d60c86eeea6a94cc1258
lukaas33 <Lucasvanosenbruggen@gmail.com>
2017-08-03 17:18:58 +0200

    Edited node files

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ var app = express();
 
 // Port
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static([__dirname, '/public'].join()));
+app.use(express.static([__dirname, '/public'].join('')));
 
 // Routes
-app.set('index', [__dirname, '/index.html'].join());
+app.set('index', [__dirname, '/index.html'].join(''));
 app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
-  response.render('index');
+  // response.render('index');
+  response.send("Hello world");
 });
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
