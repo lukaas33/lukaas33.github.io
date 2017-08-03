d7420fab13b881fc715267af3f2a0b37b2d4e0c6
lukaas33 <Lucasvanosenbruggen@gmail.com>
2017-08-03 16:43:28 +0200

    Edited node files

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var app = express();
 
+// Port
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static([__dirname, '/public'].join()));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
+// Routes
+// app.set('index', [__dirname, '/index'].join());
 app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
-  response.render('index')
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  response.render('index');
 });
