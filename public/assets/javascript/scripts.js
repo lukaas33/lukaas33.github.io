25dafb2c2f99aad8dae13493f9362c71008a2473
lukaas33 <Lucasvanosenbruggen@gmail.com>
2017-08-09 07:51:23 +0200

    Loaded scripts async

---
+++ public/assets/javascript/scripts.js
@@ -0,0 +1,17 @@
+function loadScript(url, callback) {
+  var script = document.createElement("script")
+
+  script.onload = function() {
+      callback();
+  };
+
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+};
+
+loadScript("https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js", function() {
+  console.log("Jquery loaded");
+  loadScript("assets/javascript/main.js", function() {
+    console.log("Main script loaded");
+  });
+});
