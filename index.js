6877c9f7aeae9f6773414e900e99f4034f76c08b
lukaas33 <Lucasvanosenbruggen@gmail.com>
2017-08-19 17:09:35 +0200

    Configured CSP headers to prevent XSS

--- index.js
+++ index.js
@@ -1,76 +1,91 @@
 // Require
-const express = require("express");
-const nodemailer = require("nodemailer");
-const bodyParser = require("body-parser");
-const formidable = require("formidable");
-const compression = require("compression");
-const minify = require("express-minify");
+const express = require('express')
+const csp = require('helmet-csp')
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
+const formidable = require('formidable')
+const compression = require('compression')
+const minify = require('express-minify')
 
 // Variables
-const app = express();
+const app = express()
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  service: 'Gmail',
   auth: {
     user: process.env.emailDefault,
     pass: process.env.emailPassword
   }
-});
+})
 
 // Functions
-const send = function(request, response) {
+const send = function (request, response) {
   // Handles email form
-  const form = new formidable.IncomingForm();
+  const form = new formidable.IncomingForm()
 
-  return form.parse(request, function(error, fields, files) {
+  form.parse(request, function (error, fields, files) {
     const mailOptions = {
-      from: `"${fields.email}" <${fields.email}>`,
-      to: `"Personal" <${process.env.emailPersonal}>`,
-      subject: "[SITE MESSAGE]",
+      from: `'${fields.email}' <${fields.email}>`,
+      to: `'Personal' <${process.env.emailPersonal}>`,
+      subject: '[SITE MESSAGE]',
       html:
         `<em>Sent by: ${fields.name}</em><br/>` +
         `<em>Reply at: ${fields.email}</em><br/><hr/>` +
         `${fields.message}`
-    };
+    }
 
-    return transporter.sendMail(mailOptions, function(error, info) {
-      if (error) {
-        return console.log(error);
-      } else {
-        return console.log(`Email sent: ${info.response}`);
-      }
-    });
-  });
-};
-
+    if (error) {
+      console.log("Form didn't parse")
+    } else {
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log(`Email sent: ${info.response}`)
+        }
+      })
+    }
+  })
+}
 
 // Setup
-app.use(compression()); // Compression for site
-app.use(minify()); // Minifies code
-app.use(express.static(`${__dirname}/public`)); // Serve static files
+app.use(csp({
+  directives: {
+    // Setup for http headers
+    defaultSrc: ["'self'", '*.googleapis.com'],
+    frameSrc: ['*.google.com'],
+    scriptSrc: ["'self'", "'unsafe-inline'", 'ajax.googleapis.com'],
+    styleSrc: ["'self'", "'unsafe-inline'", '*.googleapis.com'],
+    fontSrc: ["'self'", 'fonts.gstatic.com'],
+    imgSrc: ["'self'"],
+    objectSrc: ["'none'"]
+  }
+}))
 
-app.use(bodyParser.json()); // Enable json parsing
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(compression()) // Compression for site
+app.use(minify()) // Minifies code
+app.use(express.static(`${__dirname}/public`)) // Serve static files
 
-app.set("port", process.env.PORT || 5000); // Chooses either port
-app.set("index", `${__dirname}/index`);
-app.set("views", `${__dirname}/views`);
-app.set("view engine", "ejs");
-// app.set("view engine", "html")
+app.use(bodyParser.json()) // Enable json parsing
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('port', process.env.PORT || 5000) // Chooses either port
+app.set('index', `${__dirname}/index`)
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'ejs')
 
 // Routes
-app.get('/', function(request, response) {
-  response.set("Content-Encoding", "gzip");
-  // response.render("index")
-  return response.render("index");
-});
+app.get('/', function (request, response) {
+  response.set('Content-Encoding', 'gzip')
+  response.render('index')
+})
 
 // Exceptions
 
 // Events
-app.post("/send", function(request, response) {
-  console.log("POST request from client");
-  send(request, response); // Email sender
-  return response.end();
-});
+app.post('/send', function (request, response) {
+  console.log('POST request from client')
+  send(request, response) // Email sender
+  response.end()
+})
 
-app.listen(app.get("port"), () => console.log(`Node app is running at ${app.get("port")}`));
+app.listen(app.get('port'), () => console.log(`Node app is running at ${app.get('port')}`))
--- public/assets/javascript/scripts.js
+++ public/assets/javascript/scripts.js
@@ -1,74 +1,72 @@
-global = {
+var global = {
   // Variables with a global scope
   timing: 400,
   animation: {
     duration: 400,
-    easing: "swing"
+    easing: 'swing'
   },
   state: {
-    "experience": null,
-    "skills": null
+    'experience': null,
+    'skills': null
   },
   timeout: null,
   pageNum: 0,
   previous: null
 };
 
-(function() {
-
-  global.cookie = function(name, value) {
+(function () {
+  global.cookie = function (name, value) {
     // Cookie function
-    var cookies = document.cookie.split(';');
+    var cookies = document.cookie.split(';')
     if (value === void 0) {
       for (var i = 0; i < cookies.length; i++) {
-        var value = cookies[i];
-        var current = value.split('=');
+        value = cookies[i]
+        var current = value.split('=')
         if (current[0].trim() === name) {
-          return current[1];
+          return current[1]
         }
       }
     } else {
-      console.log("Set cookie " + name + " to " + value);
-      return document.cookie = name + "=" + value + "; path=/";
+      console.log('Set cookie ' + name + ' to ' + value)
+      document.cookie = name + '=' + value + '; path=/'
     }
-  };
+  }
 
-  loadScript = function(url, callback) {
+  var loadScript = function (url, callback) {
     // Loads scrips asynchronously
-    var script = document.createElement("script")
+    var script = document.createElement('script')
 
-    script.onload = function() {
-      callback();
-    };
-    script.src = url;
-    script.async = true;
-    document.getElementsByTagName("head")[0].appendChild(script);
-  };
-
-  if (global.cookie("map") === void 0) {
-    global.cookie("map", false);
-  }
-  if (global.cookie("page") === void 0) {
-    global.cookie("page", 1);
-  }
-  if (global.cookie("name") === void 0) {
-    global.cookie("name", "");
-  }
-  if (global.cookie("email") === void 0) {
-    global.cookie("email", "");
-  }
-  if (global.cookie("message") === void 0) {
-    global.cookie("message", "");
+    script.onload = function () {
+      callback()
+    }
+    script.src = url
+    script.async = true
+    document.getElementsByTagName('head')[0].appendChild(script)
   }
 
-  loadScript("https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js", function() {
-    console.log("// Jquery loaded");
-    loadScript("assets/javascript/main.js", function() {
-      console.log("// Main script loaded");
-    });
-    loadScript("assets/javascript/setup.js", function() {
-      console.log("// Setup script loaded");
-    });
-  });
+  if (global.cookie('map') === void 0) {
+    global.cookie('map', false)
+  }
+  if (global.cookie('page') === void 0) {
+    global.cookie('page', 1)
+  }
+  if (global.cookie('name') === void 0) {
+    global.cookie('name', '')
+  }
+  if (global.cookie('email') === void 0) {
+    global.cookie('email', '')
+  }
+  if (global.cookie('message') === void 0) {
+    global.cookie('message', '')
+  }
 
-}).call(this);
+  loadScript('https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', function () {
+    console.log('// Jquery loaded')
+    loadScript('assets/javascript/main.js', function () {
+      console.log('// Main script loaded')
+    })
+    loadScript('assets/javascript/setup.js', function () {
+      console.log('// Setup script loaded')
+    })
+  })
+}).call(this)
