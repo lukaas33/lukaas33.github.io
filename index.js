c4f789de9c93cc5032eb082dd110573ea7ef2526
lukaas33 <Lucasvanosenbruggen@gmail.com>
2017-08-27 20:50:25 +0200

    Generated page content for the experience section with ejs and mongodb

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const minify = require('express-minify')
 var mongoClient = require('mongodb')
 var mailgun = require('mailgun-js')
 const app = express()
+const data = {files: ['experience', 'projects', 'skills']}
 mongoClient = mongoClient.MongoClient
 mailgun = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.DOMAIN}) // Gets mailgun data from env
 
@@ -41,11 +42,6 @@ app.use(express.static(`${__dirname}/public`, { maxage: '7d' })) // Serve static
 app.engine('html', require('ejs').renderFile)
 app.set('views', `${__dirname}/views`)
 app.set('view engine', 'ejs')
-
-// Routes
-app.get('/', function (request, response) {
-  response.render('index')
-})
 
 // Functions
 const send = function (request, response) {
@@ -80,59 +76,75 @@ const send = function (request, response) {
   )
 }
 
-const data = function (entering) {
-  mongoClient.connect(process.env.MONGODB_URI, function (error, database) { // Connects to database using env info
-    const open = function (name, callback) {
-      filesystem.readFile(name, 'utf8', function (error, data) { // Open the files and store the content
-        if (error) {
-          console.log('File reading')
-          throw error
+data.set = (entering) => {
+  const open = function (name, callback) {
+    filesystem.readFile(name, 'utf8', function (error, data) { // Open the files and store the content
+      if (error) {
+        console.log('File reading')
+        throw error
+      }
+
+      let json = JSON.parse(data, function (key, value) { // Text to json
+        if ((key === 'start' || key === 'end') && value !== null) {
+          return new Date(value) // Date strings will be converted to dates
+        } else {
+          return value // Normal values
         }
-
-        let json = JSON.parse(data, function (key, value) { // Text to json
-          if ((key === 'date_start' || key === 'date_end') && value !== null) {
-            return new Date(value) // Date strings will be converted to dates
-          } else {
-            return value // Normal values
-          }
-        })
-
-        return callback(json.content) // Run function with object
       })
-    }
 
-    const insert = function (document, collection, callback) {
-      let query = {title: document.title} // Title is unique
-      database.collection(collection).updateOne(query, document, {upsert: true}, function (error, result) {
-        if (error) {
-          console.log('Database entry')
-          throw error
-        }
-        callback()
-      })
-    }
+      callback(json.content) // Run function with object
+    })
+  }
 
-    if (error) {
-      console.log('Database connect')
-      throw error
-    }
+  const upsert = function (document, collection, callback) {
+    let query = {title: document.title} // Title is unique
+    data.database.collection(collection).updateOne(query, document, {upsert: true}, function (error, result) {
+      if (error) {
+        console.log('Database entry')
+        throw error
+      }
 
-    console.log('Database connection established')
-    if (typeof entering === 'undefined') { // Will check with the local data
-      let files = ['experience', 'projects', 'skills']
+      callback()
+    })
+  }
 
-      files.forEach((file) => { // Loops through the files
-        open(`data/${file}.json`, (objects) => { // Opens the file
-          objects.forEach((object) => { // Loops through the objects
-            insert(object, file, () => { database.close() }) // Closes database
-          })
+  if (typeof entering === 'undefined') { // Will check with the local data
+    data.files.forEach((file) => { // Loops through the files
+      open(`data/${file}.json`, (objects) => { // Opens the file
+        objects.forEach((object) => { // Loops through the objects
+          upsert(object, file, () => {}) // Empty callback
         })
       })
-    } else { // Will enter new data
+    })
+  } else { // Will enter new data
 
-    }
+  }
+}
+
+data.get = function (callback) {
+  var result = {} // Will be filled
+  data.files.forEach((file, index) => {
+    let options = [{date: 1}, {date: -1}, {title: 1}] // Sort options for each file
+    data.database.collection(file).find().sort(options[index]).toArray((error, results) => {
+      if (error) {
+        console.log('Getting data')
+        throw error
+      }
+      result[file] = results
+
+      if (index >= data.files.length - 1) { // All calls completed
+        callback(result)
+      }
+    })
   })
 }
+
+// Routes
+app.get('/', function (request, response) {
+  data.get((variables) => {
+    response.render('index', {appdata: variables})
+  })
+})
 
 // Events
 app.post('/send', function (request, response) { // Post request at send
@@ -152,4 +164,12 @@ app.post('/auth', function (request, response) { // Admin authenticate
 app.listen(app.get('port'), () => console.log(`Node app is running at ${app.get('port')}`))
 
 // Actions
-data() // Database sync without entering new data
+mongoClient.connect(process.env.MONGODB_URI, function (error, database) { // Connects to database using env info
+  if (error) {
+    console.log('Database connect')
+    throw error
+  }
+  data.database = database
+  data.set() // Database sync without entering new data
+  console.log('Database connection established')
+})
