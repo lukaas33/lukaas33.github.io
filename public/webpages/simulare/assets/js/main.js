d3709699c5971bf3e2b18e0f670c4b0149529129
lukaas33 <Lucasvanosenbruggen@gmail.com>
2017-10-16 11:39:30 +0200

    Added and removed empty files

--- public/webpages/simulare/assets/js/main.js
+++ public/webpages/simulare/assets/js/main.js
@@ -1,37 +1,4 @@
 (function() {
-  var math, random;
 
-  math = {
-
-    /* More specific math object for project */
-    abs: Math.abs,
-    ceil: Math.ceil,
-    floor: Math.floor,
-    log: Math.log,
-    max: Math.max,
-    min: Math.min
-  };
-
-  random = {
-
-    /* Different functions regarding random */
-    random: Math.random(),
-    randint: function(min, max, inclusive) {
-      if (inclusive == null) {
-        inclusive = false;
-      }
-      if (inclusive) {
-        max = +1;
-      }
-      return Math.floor(Math.random() * (max - min) + min);
-    },
-    choice: function(array) {}
-  };
-
-  console.log(math.ceil(1.2));
-
-  console.log(random.randint(2, 4));
-
-  random.choice([1, 2]);
 
 }).call(this);
