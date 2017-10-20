1c6a64830a95d677acd7b293633ba86494aa0e54
lukaas33 <Lucasvanosenbruggen@gmail.com>
2017-10-20 12:38:54 +0200

    Implemented fullscreen for project pages

--- public/assets/javascript/project.js
+++ public/assets/javascript/project.js
@@ -2,3 +2,25 @@
 // TODO work out the slideshow for collections
 // TODO work out loading of content
 // TODO work out navbar
+
+// var $ = 0
+
+$(document).ready(function () {
+  // Actions
+  $('body').show()
+
+  $('.fullscreen').click(function () {
+    var $box = $('#project').find('.box')
+    if ($box.hasClass('normal')) {
+      // Set state to fullscreen
+      $box.removeClass('normal')
+      $box.addClass('fill')
+      $('nav').hide() // Can't use css for it
+    } else {
+      // Set state to normal
+      $box.removeClass('fill')
+      $box.addClass('normal')
+      $('nav').show() // Can't use css for it
+    }
+  })
+})
