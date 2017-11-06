483f15bc64614778f8cabe866d57fa658ecb5515
lukaas33 <Lucasvanosenbruggen@gmail.com>
2017-11-06 18:23:34 +0100

    PWA setup

--- public/webpages/faunadex/serviceworker.js
+++ public/webpages/faunadex/serviceworker.js
@@ -1,16 +1,37 @@
+// Serviceworker code from https://developers.google.com/web/fundamentals/primers/service-workers/
 var cacheName = 'cached-files'
 var urlsToCache = [
   'index.html',
   'manifest.json',
-  'assets/javascript/home.js'
+  'assets/javascript/home.js',
+  'assets/javascript/libraries/jquery.js',
+  'assets/javascript/libraries/ejs.js',
+  // 'assets/css/home.css'
+  // 'assets/images/other/logo-white.svg',
+  // 'assets/images/other/preview.jpg',
 ]
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+// Perform install steps
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       console.log('Opened cache')
-      cache.addAll(urlsToCache)
+      return cache.addAll(urlsToCache)
     })
+  )
+})
+
+// Return cached data
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          return response
+        }
+        return fetch(event.request)
+      }
+    )
   )
 })
