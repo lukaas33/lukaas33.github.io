0b0c4d7722187e7465111c25b134a9bbd010f8a6
lukaas33 <lucasvanosenbruggen@gmail.com>
2018-01-04 12:39:42 +0100

    Possible fix for not being able to upload images from android

--- public/webpages/faunadex/assets/javascript/scan.js
+++ public/webpages/faunadex/assets/javascript/scan.js
@@ -425,15 +425,19 @@ doc.take.click(function () {
 
 // When a new file is entered
   // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
-doc.gallery.change(function () {
-  if (validateInput(this.files)) {
+doc.gallery.change(function (event) {
+  if (validateInput(event.target.files)) {
     const reader = new FileReader() // Can read the image
     reader.onload = function (event) {
-      local.image = event.target.result // Saved as dataUrl in variable
-      imageTaken()
+      if (typeof(event.target.result) === 'string') {
+        local.image = event.target.result // Saved as dataUrl in variable
+        imageTaken()
+      } else {
+        throw Error("Input wasn't processed")
+      }
     }
 
-    reader.readAsDataURL(this.files[0]) // Read the image as a dataUrl
+    reader.readAsDataURL(event.target.files[0]) // Read the image as a dataUrl
   }
 })
 }).call(this)
