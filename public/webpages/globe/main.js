a20d16450c718b75a17b8e6560522e28c263cedb
Lucas Osenbruggen <Osenbruggen.Lucas@LCL.NL>
2018-02-08 15:10:27 +0100

    Added initial globe

--- public/webpages/globe/main.js
+++ public/webpages/globe/main.js
@@ -1,10 +1,39 @@
 // Using the three.js library
-  // Mostly used the code in the docs
+  // Copied most of the code
   // https://threejs.org/docs/
+  // http://learningthreejs.com/blog/2013/09/16/how-to-make-the-earth-in-webgl/
 
+// >> Variables
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-
 const renderer = new THREE.WebGLRenderer()
-renderer.setSize( window.innerWidth, window.innerHeight )
+
+
+// >> Functions
+ // Creates get's repeated
+const animate = function () {
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+}
+
+const draw = {}
+
+// Draws the initial scene
+draw.setup = function () {
+    const geometry = new THREE.SphereBufferGeometry(5, 32, 32)
+    const material = new THREE.MeshPhongMaterial()
+    const sphere = new THREE.Mesh(geometry, material)
+    scene.add(sphere)
+    const light = new THREE.DirectionalLight( 0xffffff )
+    light.position.set( 0, 1, 1 ).normalize()
+    scene.add(light)
+}
+
+
+// >> Actions
+// > Setup
+renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+draw.setup()
+camera.position.z = 25
+animate()
