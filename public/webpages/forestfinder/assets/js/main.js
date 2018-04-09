78854fb857a178627818108fe41c87c0999a7b9e
lukaas33 <lucasvanosenbruggen@gmail.com>
2018-04-09 13:16:58 +0200

    Changed how location is gotten

--- public/webpages/forestfinder/assets/js/main.js
+++ public/webpages/forestfinder/assets/js/main.js
@@ -1,71 +1,93 @@
+// Serviceworker register
+if ('serviceWorker' in navigator) { // If support
+  navigator.serviceWorker.register('serviceworker.js', {scope: './'}).then(function (registration) {
+    console.log('Service Worker Registered at:', registration.scope)
+  }, function (error) {
+    console.log('Registration failed', error)
+  })
+}
+
 (function () {
   'use strict'
 
-  // >> Functions
-  const getLocation = function (callback = ()=>{}, err = ()=>{}) {
-    // Track the location
-    navigator.geolocation.watchPosition ((pos)  => { // Sucess
-      if (pos.accuracy < 40) { // In meters
-        callback(pos)
-      }
-     }, (error) => { // Error
-      err() // Let know there is a problem
-
-      switch(error.code) {
-          case error.PERMISSION_DENIED:
-              alert("User denied the request for Geolocation.")
-              break
-          case error.POSITION_UNAVAILABLE:
-              alert("Location information is unavailable.")
-              break
-          case error.TIMEOUT:
-              alert("The request to get user location timed out.")
-              break
-          case error.UNKNOWN_ERROR:
-              alert("An unknown error occurred.")
-              break
-      }
-    }, { // Options
-      enableHighAccuracy: true,
-      maximumAge: 5 * 60 * 1000
-    })
-  }
-
-  const getTarget = function () {
-    return location("51.466831, 5.559329")
-  }
-
-  const location = function (string) {
-    const parts = string.split(',')
-
-    const coord = {
-      latitude: parseFloat(parts[0]),
-      longitude: parseFloat(parts[1]),
+// >> Functions
+const getLocation = function (callback, err = () => {}) {
+  const success = function (pos) { // Successfull get
+    console.log(pos)
+    if (pos.accuracy < 40) { // In meters
+      callback(pos)
     }
+   }
 
-    return coord
+   const fail = function (error) { // Error in getting
+     console.error(error)
+     err() // Let caller know there is a problem
+
+     switch(error.code) {
+         case error.PERMISSION_DENIED:
+             doc.status.innerText = "User denied the request for Geolocation."
+             break
+         case error.POSITION_UNAVAILABLE:
+             doc.status.innerText = "Location information is unavailable."
+             break
+         case error.TIMEOUT:
+             doc.status.innerText = "The request to get user location timed out."
+             break
+         case error.UNKNOWN_ERROR:
+             doc.status.innerText = "An unknown error occurred."
+             break
+     }
+   }
+
+  // Track the location
+  global.track = navigator.geolocation.watchPosition (success, fail, { // Options
+    enableHighAccuracy: true,
+    maximumAge: 5 * 60 * 1000,
+    timeout: 1 * 60 * 1000
+  })
+}
+
+const getTarget = function () {
+  return location("51.466831, 5.559329")
+}
+
+const location = function (string, acc) {
+  const parts = string.split(',')
+
+  const coord = {
+    latitude: parseFloat(parts[0]),
+    longitude: parseFloat(parts[1]),
+    accuracy: acc // In meters
   }
 
+  return coord
+}
 
 
-  // >> Classes
+
+// >> Classes
 
 
-  // >> Variables
-  const doc = {
-    status: document.getElementById('status'),
-    data: document.getElementById('data')
-  }
+// >> Variables
+const doc = {
+  status: document.getElementById('status'),
+  data: document.getElementById('data')
+}
 
-  // >> Run
-  // Check available
-  if ("geolocation" in navigator) {
-    getLocation((pos) => {
-      alert(JSON.stringify(pos.coords))
-    })
-  } else {
-    alert("Location data not available on this device.")
-  }
+const global = {
+  track: null
+}
+
+// >> Run
+// Check available
+if ("geolocation" in navigator) {
+  doc.status.innerText = "Getting location"
+  getLocation((pos) => {
+    doc.status.innerText = JSON.stringify(pos.coords)
+  })
+} else {
+  doc.status.innerText = "Location data not available on this device."
+}
 
 
-}).call(this)
+}.call(this))
