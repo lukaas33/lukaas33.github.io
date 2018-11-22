5df56d2d9b8a8d2f60f6505c7ec594d24ffe76dc
Lucas <lucasvanosenbruggen@gmail.com>
2018-11-22 11:04:26 +0100

    Added storage function

--- public/webpages/forestfinder/assets/js/database.js
+++ public/webpages/forestfinder/assets/js/database.js
@@ -2,13 +2,34 @@
 // Functions for local storage
 const database = {
   link: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQKWPQTIs8YZoVGNTRzE1iMiAmEWIsqs9xv0aBzTWIisn338KClhoAA0nuA4-8CS0b6CBjA433s2VIe/pub?gid=0&single=true&output=csv",
+  name: "treeLocationData",
   getOnline: function (callback) {
-    httpsRequest(this.link, callback)
+    getCsv(this.link, (data) => { // Get via xhttp
+      this.data = data // Store data
+      callback(data) // Return the data
+    })
+  },
+  get data () { // Accessed via database.data
+    if (window.localStorage) {
+      const stringData = localStorage.getItem(this.name)
+      const data = JSON.parse(stringData)
+      return data
+    } else {
+
+    }
+  },
+  set data (jsonData) {
+    const stringData = JSON.stringify(jsonData)
+    if (window.localStorage) {
+      localStorage.setItem(this.name, stringData)
+    } else {
+
+    }
   }
 }
 
 // Gets the target locations
-const httpsRequest = function (link, callback) {
+const getCsv = function (link, callback) {
   if (navigator.onLine) { // Internet connection
     const xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = function () { // When data returns
