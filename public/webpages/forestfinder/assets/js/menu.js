4f087c85f60cf6b0dd79e0097b88f45f5cb693fc
lukaas33 <lucasvanosenbruggen@gmail.com>
2019-01-02 19:19:51 +0100

    Worked out navigation

--- public/webpages/forestfinder/assets/js/menu.js
+++ public/webpages/forestfinder/assets/js/menu.js
@@ -1,2 +1,32 @@
 // TODO insert menu into pages
-// TODO nagigation
+const menu = {
+  nav: document.querySelector('nav'),
+  open: document.querySelector('button[name=menu]'),
+  links: null
+}
+
+
+menu.open.addEventListener('click', (event) => {
+  menu.nav.style.display = 'inline-block'
+})
+
+// Earlier solution for navigation, not needed because of base tag in html
+  // menu.links = document.querySelectorAll('nav a')
+  // for (let link of menu.links) {
+  //   link.addEventListener('click', (event) => {
+  //     event.preventDefault() // No normal link behaviour
+  //     for (let element of event.path) {
+  //       if (element.localName === 'a') { // Found link element
+  //         let url = element.attributes[0].baseURI
+  //
+  //         // Normalise path
+  //         for (let pageName of ['herbarium', 'opties']) {
+  //           url = url.split(pageName)[0]
+  //         }
+  //
+  //         url = url + element.attributes[0].nodeValue
+  //         window.location = url // Redirect
+  //       }
+  //     }
+  //   })
+  // }
