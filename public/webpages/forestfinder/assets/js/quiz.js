071c401948b6a80f4cbf0786e61f144c78a1f3a5
lukaas33 <lucasvanosenbruggen@gmail.com>
2019-02-14 17:19:31 +0100

    Worked on quiz

--- public/webpages/forestfinder/assets/js/quiz.js
+++ public/webpages/forestfinder/assets/js/quiz.js
@@ -9,20 +9,45 @@ const quiz = {
       "lines": ['Veernervig', 'Handnervig', 'Parallelnervig']
     }
   },
+  get questions () { // Get only
+    return {
+      "form": "Welke vorm heeft dit blad?",
+      "edge": "Wat voor bladranden heeft deze boom?",
+      "lines": "Welke nervatuur heeft het blad?"
+    }
+  },
 
-  answers: [],
+  answers: {
+    "form": null,
+    "edge": null,
+    "lines": null
+  },
 
   start (info) {
     for (let question of this.question_names) {
       let answer = info[`quiz_${question}`]
-      this.answers.push(answer)
+      this.answers[question] = answer
     }
 
-
+    this.setup(info)
   },
-  setup () {
-    for (let question of this.question_names) {
+  setup (info) {
+    const container = document.querySelector("#overlay")
+    const box = document.createElement('div')
+
+    const name = document.createElement('h3')
+    name.textContent = info.name
+    box.appendChild(name)
+
+    for (let question of this.question_names) { // All questions
+      const part = document.createElement('div')
+      const text = document.createElement('p')
+      text.textContent = this.questions[question]
+
+      part.appendChild(text)
+
       let select = document.createElement("select")
+      select.appendChild(document.createElement('option')) // Empty input option
       for (let answer of this.possible_answers[question]) {
         let option = document.createElement('option')
         option.value = answer.toLowerCase()
@@ -30,10 +55,26 @@ const quiz = {
         select.appendChild(option)
       }
       select.name = question
+
+      select.addEventListener('input', this.check) // When user selects an option
+
+      part.appendChild(select)
+      box.appendChild(part)
     }
+
+    container.appendChild(box)
+    container.style.display = 'block'
   },
 
-  check (name, choice) {
+  check (event) {
+    const options = event.srcElement.options
+    const value = options[options.selectedIndex].value
+    const question = event.srcElement.name
 
+    if (value === this.answers[question]) {
+      
+    }
+
+    event.srcElement.style.display = 'none'
   }
 }
