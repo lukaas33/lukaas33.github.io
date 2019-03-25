e239701ecc3d6372e9fedfa5dbeaefc2b6e9219a
Lucas <lucasvanosenbruggen@gmail.com>
2019-03-25 13:00:33 +0100

    Started on export download

--- public/webpages/forestfinder/assets/js/export.js
+++ public/webpages/forestfinder/assets/js/export.js
@@ -1,1 +1,22 @@
+const saveToHtml = function () {
+  const content = [document.querySelector('html').innerHTML]
+  const file = new Blob(content, {type: "text/html"}) // JS file object
+  const link = document.createElement('a')
+  link.href = URL.createObjectURL(file) // Save to Url
+  link.download = "forestfinder-export.html" // File name
+  link.hidden = true // Not visible
+  document.body.appendChild(link) // Needs to be in the DOM
+  link.innerHTML = 'Download'
+  link.click() // Start download
+}
 
+for (let script of document.getElementsByTagName('script')) {
+  script.parentNode.removeChild(script)
+}
+
+const argument = location.href.split('?action=')[1]
+if (argument === 'save') {
+  saveToHtml()
+} else if (argument === 'mail') {
+
+}
