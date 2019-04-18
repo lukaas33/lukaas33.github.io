4df5e1dcf5bf98d645c9b984d2f63da4557c0084
lukaas33 <lucasvanosenbruggen@gmail.com>
2019-04-18 21:53:26 +0200

    Added Favicon

--- public/webpages/forestfinder/assets/js/opties.js
+++ public/webpages/forestfinder/assets/js/opties.js
@@ -28,10 +28,10 @@ document.querySelector("button[name=reset]").addEventListener('click', (event) =
 })
 
 // Not used anymore
-document.querySelector("button[name=mail]").addEventListener('click', (event) => {
-  this.disabled = true // No clicking twice
-  location.href = 'assets/elements/export.html/?action=mail'
-})
+// document.querySelector("button[name=mail]").addEventListener('click', (event) => {
+//   this.disabled = true // No clicking twice
+//   location.href = 'assets/elements/export.html/?action=mail'
+// })
 
   document.querySelector("button[name=save]").addEventListener('click', (event) => {
     this.disabled = true // No clicking twice
